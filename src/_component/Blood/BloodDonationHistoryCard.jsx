603473ff5a6d51b7f50ed1e9e0b50d93cfe4763a
Muhammed Javad <muhammedjavad@Muhammeds-MacBook-Air.local>
2024-05-19 20:45:45 +0530

    Fund donation history card

--- src/_component/Blood/BloodDonationHistoryCard.jsx
+++ src/_component/Blood/BloodDonationHistoryCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import DownloadButton from '../Util/downloadButton'
 
 function BloodDonationHistoryCard() {
     return (
@@ -25,13 +26,9 @@ function BloodDonationHistoryCard() {
                 </div>
                 <div className="w-2/6">
                     <div className='flex w-full items-center justify-end w-full h-full'>
-                        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
-                            <span>Download Certificate</span>
-                        </button>
+                        <DownloadButton title={"Download Certificate"} onClick={() => { }} />
                     </div>
                 </div>
-
 
             </div>
 
