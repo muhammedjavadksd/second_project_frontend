46e40fea2b5d4177fc2bff19ca111f5680f90a32
Muhammed Javad <muhammedjavad@Muhammeds-MacBook-Air.local>
2024-07-03 09:31:28 +0530

    Admin sidebar updated

--- src/_component/Admin/Partials/NavLinks/LinkNav.jsx
+++ src/_component/Admin/Partials/NavLinks/LinkNav.jsx
@@ -3,12 +3,10 @@ import React from 'react'
 
 function LinkNav({ title, icon, href, isActive, isSub }) {
     return (
-        <li>
-            <Link href={href} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                 {icon}
-                <span class="ms-3">{title}</span>
-            </Link>
-        </li>
+        <Link href={href} class="flex text-sm items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+            {icon}
+            <span class="ms-3">{title}</span>
+        </Link>
     )
 }
 
