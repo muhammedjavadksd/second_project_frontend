90d9b941cddb7e907feae00cb4b3add893ec85c4
muhammedjavadksd <javadjazz899@gmail>
2024-07-25 09:46:30 +0530

    Created seperete folders to yups

---
+++ src/util/external/yup/initialValues.ts
@@ -0,0 +1,11 @@
+
+
+const bloodDonatationFormValues = {
+    full_name: '',
+    phone_number: '',
+    email_address: '',
+    location: '',
+    blood_group: '',
+}
+
+export { bloodDonatationFormValues }
---
+++ src/util/external/yup/yupValidations.ts
@@ -0,0 +1,13 @@
+
+import { BloodGroup } from '@/util/types/Enums/BasicEnums'
+import * as yup from 'yup'
+
+const bloodDonatationFormValidation = yup.object().shape({
+    full_name: yup.string().typeError("Please enter valid full name").required("Full name is required"),
+    phone_number: yup.number().typeError("Please enter valid phone number").required("Phone number is required"),
+    email_address: yup.string().email("Please enter valid email address").typeError("Please enter valid email address").required("Email address is required"),
+    location: yup.string().optional(),
+    blood_group: yup.string().typeError("Please select valid blood group").oneOf(Object.values(BloodGroup), "Please select valid blood group").required("Blood group is required")
+})
+
+export { bloodDonatationFormValidation }
