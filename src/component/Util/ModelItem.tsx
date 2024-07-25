fa1b3aeb536e4ce0900a127b49ed9e7c2075899c
muhammedjavadksd <javadjazz899@gmail>
2024-07-25 09:46:14 +0530

    Model item component (Reusable)

---
+++ src/component/Util/ModelItem.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+function ModelItem({ children, isOpen, onClose, ZIndex }) {
+
+    let [isModelOpen, setModelOpen] = useState<boolean>(false)
+
+    useEffect(() => {
+        setModelOpen(isOpen ? true : false)
+    }, [isOpen])
+
+    function closeThisModel(): void {
+        setModelOpen(false)
+        onClose()
+    }
+
+    return (
+        <>
+
+            <div style={{ zIndex: ZIndex }} onClick={() => closeThisModel()} className={`${!isModelOpen && "hidden"}  top-0 left-0 right-0 bg-black bg-opacity-80 flex items-center justify-center fixed w-full h-screen`}>
+                <div className="modelScreen" onClick={(e) => e.stopPropagation()}>
+                    {children}
+                </div>
+            </div></>
+    )
+}
+
+export default ModelItem
