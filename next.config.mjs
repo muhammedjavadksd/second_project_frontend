b875f649cdfddac7b2fa01007bb06c093cb6e670
muhammedjavadksd <javadjazz899@gmail>
2024-10-02 13:16:45 +0530

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // experimental: {
-    //     appDir: true,
-    // },
+    experimental: {
+        appDir: true,
+        async headers() {
+            return [
+                {
+                    source: "/api/:path*",
+                    headers: [
+                        { key: "Access-Control-Allow-Credentials", value: "true" },
+                        { key: "Access-Control-Allow-Origin", value: "https://life-link.online" },
+                        {
+                            key: "Access-Control-Allow-Methods",
+                            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                        },
+                        {
+                            key: "Access-Control-Allow-Headers",
+                            value:
+                                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                        },
+                    ],
+                },
+            ];
+        },
+    },
     env: {
         PUBLIC_IMAGE_URL: "/images"
     },
