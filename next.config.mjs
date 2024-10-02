f4dde98fcc5e9760329f3ee595f99d6743ae6816
muhammedjavadksd <javadjazz899@gmail>
2024-10-02 13:51:15 +0530

    cors updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
+    async rewrites() {
         return [
             {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-                    },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value:
-                            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-                    },
-                ],
+                source: '/api/:path*',
+                destination: 'https://life-link.online/api/:path*', // keep the same path structure
             },
-        ];
+        ]
     },
     env: {
         PUBLIC_IMAGE_URL: "/images"
