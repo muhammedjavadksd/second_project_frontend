fdde56716aa68fe315f1f1ce1144fd0bef426191
muhammedjavadksd <javadjazz899@gmail>
2024-10-09 20:54:38 +0530

    error resolved

--- src/util/data/hook.ts
+++ src/util/data/hook.ts
@@ -3,19 +3,25 @@
 
 export function useGetLocation(state) {
 
-    if (navigator.geolocation) {
-        try {
-            navigator.geolocation.getCurrentPosition(({ coords }) => {
-                state([coords.longitude, coords.latitude])
-            }, (err) => {
-                console.log(err);
-            })
-        } catch (e) {
+    if (typeof window !== 'undefined') {
+        if (navigator.geolocation) {
+            try {
+                navigator.geolocation.getCurrentPosition(({ coords }) => {
+                    state([coords.longitude, coords.latitude])
+                }, (err) => {
+                    console.log(err);
+                })
+                return navigator
+            } catch (e) {
+                console.log('Geolocation is not supported by this browser.');
+            }
+        } else {
             console.log('Geolocation is not supported by this browser.');
         }
-    } else {
-        console.log('Geolocation is not supported by this browser.');
+
     }
 
-    return navigator
+    return null
+
+
 }
