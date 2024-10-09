2ec270255b8c36fe28a9fa52dd0983657f294515
muhammedjavadksd <javadjazz899@gmail>
2024-10-09 20:57:56 +0530

    global navigator

--- src/util/data/hook.ts
+++ src/util/data/hook.ts
@@ -4,21 +4,17 @@
 export function useGetLocation(state) {
 
     if (typeof window !== 'undefined') {
-        if (navigator.geolocation) {
-            try {
-                navigator.geolocation.getCurrentPosition(({ coords }) => {
-                    state([coords.longitude, coords.latitude])
-                }, (err) => {
-                    console.log(err);
-                })
-                return navigator
-            } catch (e) {
-                console.log('Geolocation is not supported by this browser.');
-            }
-        } else {
+
+        try {
+            global.navigator.geolocation.getCurrentPosition(({ coords }) => {
+                state([coords.longitude, coords.latitude])
+            }, (err) => {
+                console.log(err);
+            })
+            return global.navigator
+        } catch (e) {
             console.log('Geolocation is not supported by this browser.');
         }
-
     }
 
     return null
